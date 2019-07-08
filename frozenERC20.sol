da30d39763321475a6bae8e357fe7366defe10a2
foobar94 <42718531+foobar94@users.noreply.github.com>
2019-07-08 16:09:04 -0400

    minimalize
    remove unnecessary function/event

--- frozenERC20.sol
+++ frozenERC20.sol
@@ -7,10 +7,6 @@ pragma solidity ^0.4.17;
 contract Owned {
 	modifier only_owner { require (msg.sender == owner); _; }
 
-	event NewOwner(address indexed old, address indexed current);
-
-	function setOwner(address _new) public only_owner { NewOwner(owner, _new); owner = _new; }
-
 	address public owner;
 }
 
