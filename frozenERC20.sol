27cc981188034e0a8d2ba4abce7901e1e640edbc
foobar94 <42718531+foobar94@users.noreply.github.com>
2019-07-09 13:09:19 -0400

    fixes for newer solidity version

--- frozenERC20.sol
+++ frozenERC20.sol
@@ -1,24 +1,16 @@
 //! FrozenToken ECR20-compliant token contract
-
-
-pragma solidity ^0.4.17;
-
+pragma solidity >=0.4.22 <0.7.0;
 // Owned contract.
 contract Owned {
 	modifier only_owner { require (msg.sender == owner); _; }
-
 	address public owner;
 }
 
-// FrozenToken, a bit like an ECR20 token (though not - as it doesn't
-// implement most of the API).
-// All token balances are generally non-transferable.
-// All "tokens" belong to the owner (who is uniquely liquid) at construction.
+// All "tokens" belong to the owner at construction.
 contract FrozenToken is Owned {
 	event Transfer(address indexed from, address indexed to, uint256 value);
-
 	// constructor sets the parameters of execution, _totalSupply is all units
-	function FrozenToken(uint _totalSupply, address _owner)
+	constructor(uint _totalSupply, address _owner)
         public
 		when_non_zero(_totalSupply)
 	{
@@ -26,44 +18,35 @@ contract FrozenToken is Owned {
 		owner = _owner;
 		accounts[_owner] = totalSupply;
 	}
-
 	// balance of a specific address
-	function balanceOf(address _who) public constant returns (uint256) {
+	function balanceOf(address _who) public view returns (uint256) {
 		return accounts[_who];
 	}
-
 	// transfer
 	function transfer(address _to, uint256 _value)
 		public
 		only_owner
 		returns(bool)
 	{
-		Transfer(msg.sender, _to, _value);
+		emit Transfer(msg.sender, _to, _value);
 		accounts[msg.sender] -= _value;
 		accounts[_to] += _value;
-
 		return true;
 	}
-
-	// no default function, simple contract only, entry-level users
-	function() public {
+	function() external {
 		assert(false);
 	}
-
 	// a value should be > 0
 	modifier when_non_zero(uint _value) {
 		require (_value > 0);
 		_;
 	}
-
 	// Available token supply
 	uint public totalSupply;
-
 	// Storage and mapping of all balances & allowances
 	mapping (address => uint256) accounts;
-
 	// Conventional metadata.
 	string public constant name = "AVA Allocation Indicator";
 	string public constant symbol = "AVA";
-	uint8 public constant decimals = 3;
+	uint8 public constant decimals = 18;
 }
